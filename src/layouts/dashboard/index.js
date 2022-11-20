030603ce4c464ab9b8e8a3567932ae4533c0b82c
Muhammad Mahad <muhammad_mahad@mentor.com>
2022-11-20 16:39:32 +0500

    [React]: Chat App
    - complete chat list
    - fix node package issues

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,7 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { Avatar, Box, Divider, IconButton, Stack, Switch } from "@mui/material";
 import { useTheme, styled } from "@mui/material/styles";
-import { Gear } from "phosphor-react";
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import logo from "../../assets/Images/logo.ico"
@@ -56,7 +55,7 @@ const DashboardLayout = () => {
   const [selected, setSelected] = useState(0);
   const { onToggleMode } = useSettings();
   const navIcons = (el) => (
-    el.index == selected ?
+    el.index === selected ?
       <Box sx={{
         backgroundColor: theme.palette.primary.main,
         borderRadius: 1.5
@@ -77,36 +76,38 @@ const DashboardLayout = () => {
 
   return (
     <>
-      <Box
-        p={2}
-        sx={{ backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width: 100 }}>
-        <Stack direction={"column"} alignItems={"center"} height={"100%"} justifyContent={"space-between"} spacing={2}>
-          <Stack alignItems={"center"} width={"100%"} spacing={4}>
-            <Box sx={{
-              backgroundColor: theme.palette.primary.main,
-              height: 64,
-              width: 64,
-              borderRadius: 1.5
-            }}>
-              <img src={logo} alt={"Chat App"} />
-            </Box>
-            <Stack sx={{ width: "max-content" }} alignItems={"center"} direction={"column"} spacing={3}>
-              {Nav_Buttons.map((el) => navIcons(el))}
-              <Divider sx={{ width: "48px" }} />
-              {Nav_Setting.map((el) => navIcons(el))}
+      <Stack direction="row">
+        <Box
+          p={2}
+          sx={{ backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width: 100 }}>
+          <Stack direction={"column"} alignItems={"center"} height={"100%"} justifyContent={"space-between"} spacing={2}>
+            <Stack alignItems={"center"} width={"100%"} spacing={4}>
+              <Box sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: 1.5
+              }}>
+                <img src={logo} alt={"Chat App"} />
+              </Box>
+              <Stack sx={{ width: "max-content" }} alignItems={"center"} direction={"column"} spacing={3}>
+                {Nav_Buttons.map((el) => navIcons(el))}
+                <Divider sx={{ width: "48px" }} />
+                {Nav_Setting.map((el) => navIcons(el))}
+              </Stack>
+            </Stack>
+            <Stack spacing={3}>
+              <AntSwitch onChange={() => {
+                onToggleMode();
+              }
+              }
+                defaultChecked />
+              <Avatar src={faker.image.avatar()} />
             </Stack>
           </Stack>
-          <Stack spacing={3}>
-            <AntSwitch onChange={() => {
-              onToggleMode();
-            }
-            }
-              defaultChecked />
-            <Avatar src={faker.image.avatar()} />
-          </Stack>
-        </Stack>
-      </Box>
-      <Outlet />
+        </Box>
+        <Outlet />
+      </Stack>
     </>
   );
 };
